49be49075800593b30cd974dcf7668303ecff12c
atharva009 <atharvahankare@gmail.com>
2025-04-02 16:07:11 -0400

    add neighborhood sorter

---
+++ src/util/ADTTreeMap/INeighborhoodSorter.java
@@ -0,0 +1,26 @@
+package util.ADTTreeMap;
+
+import java.util.List;
+
+import model.Neighborhood;
+
+public interface INeighborhoodSorter {
+	
+	/**
+     * Inserts a neighborhood into the sorter based on the configured metric.
+     * @param neighborhood the neighborhood to insert
+     */
+    void insertNeighborhood(Neighborhood neighborhood);
+
+    /**
+     * Retrieves a list of all neighborhoods in sorted order.
+     * @return a sorted list of neighborhoods
+     */
+    List<Neighborhood> getSortedNeighborhoods();
+
+    /**
+     * Prints the sorted list of neighborhoods to the console.
+     */
+    void displaySortedNeighborhoods();
+    
+}
